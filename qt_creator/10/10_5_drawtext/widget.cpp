d5400ffc65d9034ac94576bc17ea9376dc7fc652
mi546870961 <chenyushen1989@163.com>
2018-03-12 10:46:14 +0800

    drawtext
    drawtext

---
+++ qt_creator/10/10_5_drawtext/widget.cpp
@@ -0,0 +1,35 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QPainter>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QRectF rect(10.0,10.0,380.0,280.0);
+    painter.setPen(Qt::red);
+    painter.drawRect(rect);
+    painter.setPen(Qt::blue);
+    painter.drawText(rect,Qt::AlignHCenter,tr("AlignHCeter"));
+    painter.drawText(rect,Qt::AlignLeft,tr("AlignLeft"));
+    painter.drawText(rect,Qt::AlignRight,tr("AlignRight"));
+    painter.drawText(rect,Qt::AlignVCenter,tr("AlignVCenter"));
+    painter.drawText(rect,Qt::AlignBottom,tr("AlignBottom"));
+    painter.drawText(rect,Qt::AlignCenter,tr("AlignCenter"));
+    painter.drawText(rect,Qt::AlignBottom|Qt::AlignRight,tr("AlignBottom\nAlignRight"));
+    QFont font("宋体",15,QFont::Bold,true);
+    font.setUnderline(true);
+    font.setOverline(true);
+
+}
